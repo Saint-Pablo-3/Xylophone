9be945e74102df245d962a3c4daa0f411502e947
Pablo <pawellreshet@icloud.com>
2023-01-24 20:32:27 +0300

    first main commit of complete app

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -2,18 +2,39 @@
 //  ViewController.swift
 //  Xylophone
 //
-//  Created by админ on 24.01.2023.
+//  Created by Pavel Reshetov on 24.01.2023.
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(forResource: sender.currentTitle!)
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) { sender.alpha = 1.0 }
+    }
+    
+    func playSound(forResource: String) {
+        guard let url = Bundle.main.url(
+            forResource: forResource,
+            withExtension: "wav") else {
+            return
+        }
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+        } catch {
+            print("sound error")
+        }
+        player?.play()
+    }
+    
 }
-
